b6473a0b27f19e02437013b259ea97fedc2d2821
fulano <fulano@zutano.com>
2023-09-03 19:54:57 +0200

    muchoTokens with public list of holders

--- contracts/MuchoTokens.sol
+++ contracts/MuchoTokens.sol
@@ -34,12 +34,15 @@ import "../interfaces/IMuchoToken.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 
 contract MuchoToken is IMuchoToken, ERC20, Ownable {
     using SafeMath for uint256;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
     uint8 decim = 18;
+    EnumerableSet.AddressSet holdersSet;
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
         decim = decimals_;
@@ -47,14 +50,38 @@ contract MuchoToken is IMuchoToken, ERC20, Ownable {
  
     function burn(address _from, uint256 _amount) external onlyOwner  {
         _burn(_from, _amount);
+
+        if(balanceOf(_from) == 0)
+            holdersSet.remove(_from);
     }
 
     function mint(address recipient, uint256 _amount) external onlyOwner {
         _mint(recipient, _amount);
+        
+        if(balanceOf(recipient) > 0)
+            holdersSet.add(recipient);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal virtual override {
+        ERC20._transfer(from, to, amount);
+
+        if(balanceOf(from) == 0)
+            holdersSet.remove(from);
+
+        if(balanceOf(to) > 0)
+            holdersSet.add(to);
     }
 
     function decimals() public view override(ERC20, IERC20Metadata) returns (uint8) {
         return decim;
+    }
+
+    function holders() public view returns (address[] memory){
+        address[] memory out = new address[](holdersSet.length());
+        for(uint256 i = 0; i < holdersSet.length(); i++){
+            out[i] = holdersSet.at(i);
+        }
+        return out;
     }
 
 }
